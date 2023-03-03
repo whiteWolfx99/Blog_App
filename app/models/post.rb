672c89f0753467a3d4cef4d0e295e38a6ead8648
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2023-03-03 14:42:48 +0300

    fix requirments

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,8 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   validates :title, :text, presence: true
+
+  def recentcomments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
